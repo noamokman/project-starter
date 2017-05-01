6f8f7c6ac2ee2d0fdef731240c1fe40261b399ac
Noam Okman <noamokman@gmail.com>
2017-05-01 12:39:30 +0300

    refactor(login): refactor validation logic

--- client/App/Login/Login.js
+++ client/App/Login/Login.js
@@ -18,14 +18,9 @@ const Login = ({handleSubmit}) => (
 );
 
 const validate = values => {
-  const errors = {};
-  const fields = ['email', 'password'];
-
-  fields.forEach(field => {
-    if (!values[field]) {
-      errors[field] = 'Required';
-    }
-  });
+  const errors = ['email', 'password'].reduce((acc, field) => {
+    return !values[field] ? {...acc, [field]: 'Required'} : acc;
+  }, {});
 
   if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = 'Invalid email address';
