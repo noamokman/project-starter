49d51c9e3cff0e2cd47ac07bc99de21cad443c17
Noam Okman <noamokman@gmail.com>
2017-07-12 22:39:41 +0300

    test(server): mongoose config

---
+++ __tests__/server/config/mongoose.spec.js
@@ -0,0 +1,97 @@
+const mockInfo = jest.fn();
+const mockError = jest.fn();
+const mockSeed = jest.fn();
+
+jest.mock('env-bunyan', () => ({
+  info: mockInfo,
+  error: mockError
+}));
+
+jest.mock('mongoose-plugin-seed', () => ({
+  seed: mockSeed
+}));
+
+describe('Mongoose config', () => {
+  describe('exports', () => {
+    it('should expose default function', () => {
+      const module = require('../../../server/config/mongoose');
+
+      expect(module).toHaveProperty('default');
+      expect(typeof module.default).toBe('function');
+    });
+  });
+
+  describe('seed function', () => {
+    beforeEach(() => {
+      mockInfo.mockReset();
+      mockError.mockReset();
+      mockSeed.mockReset();
+    });
+
+    it('should return a promise on SEED_DB undefined', () => {
+      const {default: seed} = require('../../../server/config/mongoose');
+
+      const promise = seed();
+
+      expect(promise).toBeInstanceOf(Promise);
+
+      return promise.then(() => {
+        expect(mockError).not.toBeCalled();
+        expect(mockInfo).not.toBeCalled();
+        expect(mockSeed).not.toBeCalled();
+      });
+    });
+
+    it('should return a promise on SEED_DB=false', () => {
+      process.env.SEED_DB = 'false';
+
+      const {default: seed} = require('../../../server/config/mongoose');
+
+      const promise = seed();
+
+      expect(promise).toBeInstanceOf(Promise);
+
+      return promise.then(() => {
+        expect(mockError).not.toBeCalled();
+        expect(mockInfo).not.toBeCalled();
+        expect(mockSeed).not.toBeCalled();
+      });
+    });
+
+    it('should call seed on SEED_DB=true', () => {
+      process.env.SEED_DB = 'true';
+      mockSeed.mockImplementation(() => Promise.resolve());
+
+      const {default: seed} = require('../../../server/config/mongoose');
+
+      const promise = seed();
+
+      expect(promise).toBeInstanceOf(Promise);
+
+      return promise.then(() => {
+        expect(mockSeed).toBeCalled();
+        expect(mockInfo).toBeCalledWith('Finished populating database.');
+        expect(mockError).not.toBeCalled();
+      });
+    });
+
+    it('should log seed errors', () => {
+      process.env.SEED_DB = 'true';
+      const err = new Error('lol');
+
+      mockSeed.mockImplementation(() => Promise.reject(err));
+
+      const {default: seed} = require('../../../server/config/mongoose');
+
+      const promise = seed();
+
+      expect(promise).toBeInstanceOf(Promise);
+
+      return promise.then(() => {
+        expect(mockSeed).toBeCalled();
+        expect(mockInfo).not.toBeCalled();
+        expect(mockError).toBeCalledWith({err}, 'Unable to populate database');
+      });
+    });
+  });
+});
