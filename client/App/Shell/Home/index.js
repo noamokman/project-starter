e9e5245d3d6feebf5b6ecfdef394d7efb334a1c6
Noam Okman <noamokman@gmail.com>
2017-07-13 16:30:34 +0300

    feat(client): added app bar

--- client/App/Shell/Home/index.js
+++ client/App/Shell/Home/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import {AppBar, Card, CardText, CardTitle} from 'material-ui';
 import NewTodoInput from './NewTodoInput';
 import TodoList from './TodoList';
 import {Flex} from 'reflexbox';
 
 export default () => (
-  <Flex auto column align='center' justify='center'>
-    <h1>{'Todos'}</h1>
-    <NewTodoInput />
-    <TodoList />
+  <Flex auto column>
+    <AppBar title='Project Starter' showMenuIconButton={false} />
+    <Flex auto column align='center' justify='center'>
+      <Card>
+        <CardTitle title='Todos' />
+        <CardText>
+          <NewTodoInput />
+          <TodoList />
+        </CardText>
+      </Card>
+    </Flex>
   </Flex>
 );
