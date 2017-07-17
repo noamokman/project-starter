69d8c38d2f3a52eb533b86738773dd8cd10829e2
Noam Okman <noamokman@gmail.com>
2017-07-17 16:26:17 +0300

    feat(client): logout icon on appbar

--- client/App/Shell/index.js
+++ client/App/Shell/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {AppBar} from 'material-ui';
+import {AppBar, IconButton} from 'material-ui';
 import Layout from '../components/Layout';
+import ActionExitToApp from 'material-ui/svg-icons/action/exit-to-app';
+
+const logout = (
+  <IconButton tooltip='Logout'>
+    <ActionExitToApp />
+  </IconButton>
+);
 
 export default ({children}) => (
   <Layout>
-    <AppBar title='Project Starter' showMenuIconButton={false} />
+    <AppBar title='Project Starter' showMenuIconButton={false} iconElementRight={logout} />
     {children}
   </Layout>
 );
