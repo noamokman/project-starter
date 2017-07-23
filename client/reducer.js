6b15921b65fae2010d2848a94d61066bd718ff47
Noam Okman <noamokman@gmail.com>
2017-07-23 15:16:41 +0300

    feat(client): remove redux async connect

--- client/reducer.js
+++ client/reducer.js
@@ -1,13 +1,11 @@
 import {combineReducers} from 'redux';
 import {routerReducer as routing} from 'react-router-redux';
-import {reducer as reduxAsyncConnect} from 'redux-async-connect';
 import {reducer as form} from 'redux-form';
 import auth from './reducers/auth';
 import home from './App/Shell/Home/redux';
 
 export default combineReducers({
   routing,
-  reduxAsyncConnect,
   form,
   auth,
   home
--- client/routes.js
+++ client/routes.js
@@ -1,26 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Router, Route, Redirect} from 'react-router';
-import {ReduxAsyncConnect} from 'redux-async-connect';
 import App from './App';
 import shellRoutes from './App/Shell/routes';
 import exteriorRoutes from './App/Exterior/routes';
 
-export default class Routes extends Component {
-  static renderRouter (props) {
-    return (<ReduxAsyncConnect {...props} />);
-  }
-
-  render () {
-    const {history} = this.props;
-
-    return (
-      <Router history={history} render={this.renderRouter}>
-        <Route path='/' component={App}>
-          {shellRoutes}
-          {exteriorRoutes}
-          <Redirect from='*' to='/' />
-        </Route>
-      </Router>
-    );
-  }
-}
+export default ({history}) => (
+  <Router history={history}>
+    <Route path='/' component={App}>
+      {shellRoutes}
+      {exteriorRoutes}
+      <Redirect from='*' to='/' />
+    </Route>
+  </Router>
+);
