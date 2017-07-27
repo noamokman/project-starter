55b5cd48cdfb5716308e0d27e1c088c221d06da7
Cohen Roy <cohenroy1993@gmail.com>
2017-07-27 15:01:52 +0300

    fix(client): TodoList overflow

--- client/App/Shell/Home/TodoList/TodoList.js
+++ client/App/Shell/Home/TodoList/TodoList.js
@@ -2,18 +2,28 @@ import React from 'react';
 import TodoItem from './components/TodoItem';
 import plur from 'plur';
 import {List} from 'material-ui';
+import styled from 'styled-components';
+import {Flex} from 'reflexbox';
+
+const ScrollableDiv = styled(Flex)
+  .attrs({column: true})`
+    overflow-y: auto;
+    max-height: 50vh;
+`;
 
 export default ({todos, completeTodo, deleteTodo, activeTodos}) => (
   <div>
     <List>
-      {todos.map(todo => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          completeTodo={completeTodo}
-          deleteTodo={deleteTodo}
-        />
-      ))}
+      <ScrollableDiv>
+        {todos.map(todo => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            completeTodo={completeTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </ScrollableDiv>
     </List>
     <span>{activeTodos ? `${activeTodos} ${plur('item', activeTodos)} left` : 'No items left'}</span>
   </div>
