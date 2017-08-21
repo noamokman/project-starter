104d9ed2bc57677d89c1525de0b3da248642c5ee
Sharon Grossman <sharong1337@gmail.com>
2017-08-21 16:05:47 +0300

    test(App): added App component tests

--- __tests__/client/App/index.spec.js
+++ __tests__/client/App/index.spec.js
@@ -6,4 +6,19 @@ describe('App component', () => {
   it('renders without crashing', () => {
     shallow(<App />);
   });
+
+  it('should not render DevTools in production', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('DevTools')).toBePresent();
+
+    const env = process.env.NODE_ENV;
+
+    process.env.NODE_ENV = 'production';
+
+    const productionWrapper = shallow(<App />);
+
+    expect(productionWrapper.find('DevTools')).toBeEmpty();
+    process.env.NODE_ENV = env;
+  });
 });
