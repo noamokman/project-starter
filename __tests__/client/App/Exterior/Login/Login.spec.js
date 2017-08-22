07e9369553270b5ef87b990614464292d646f792
Sharon Grossman <sharong1337@gmail.com>
2017-08-22 16:48:40 +0300

    test(components): added tests for Login and CompleteAllButton components

--- __tests__/client/App/Exterior/Login/Login.spec.js
+++ __tests__/client/App/Exterior/Login/Login.spec.js
@@ -6,4 +6,11 @@ describe('Login component', () => {
   it('renders without crashing', () => {
     shallow(<Login />);
   });
+
+  it('should render a SnackBar if an error occurs', () => {
+    const wrapper = shallow(<Login loginError={'error'} />);
+
+    expect(wrapper.find('Snackbar')).toHaveLength(1);
+    expect(wrapper.find('Snackbar')).toHaveProp('message', 'error');
+  });
 });
--- __tests__/client/App/Shell/Home/CompleteAllButton/CompleteAllButton.spec.js
+++ __tests__/client/App/Shell/Home/CompleteAllButton/CompleteAllButton.spec.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import CompleteAllButton from '../../../../../../client/App/Shell/Home/CompleteAllButton/CompleteAllButton';
+import {blue500} from 'material-ui/styles/colors';
 
 describe('CompleteAllButton component', () => {
   it('renders without crashing', () => {
     shallow(<CompleteAllButton />);
+  });
+
+  it('shows the correct tooltip and color if checked', () => {
+    const wrapper = shallow(<CompleteAllButton checked />);
+
+    expect(wrapper.find('IconButton')).toHaveProp('tooltip', 'Mark all unchecked');
+    expect(wrapper.find('ActionDoneAll')).toHaveProp('color', blue500);
   });
 });
