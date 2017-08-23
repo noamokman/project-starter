da5a0c010cf779f88f1197eb0b9143e517934a1e
Noam Okman <noamokman@gmail.com>
2017-08-23 11:21:19 +0300

    test(client): AppBar container

--- __tests__/client/App/Shell/components/AppBar/index.spec.js
+++ __tests__/client/App/Shell/components/AppBar/index.spec.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import AppBar from '../../../../../../client/App/Shell/components/AppBar';
 import configureMockStore from 'redux-mock-store';
+import {object} from 'prop-types';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 
 describe('AppBar container', () => {
   it('renders without crashing', () => {
@@ -9,5 +12,31 @@ describe('AppBar container', () => {
     const store = mockStore();
 
     shallow(<AppBar store={store} />);
+  });
+
+  it('should handle click', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore({auth: {}});
+    const muiTheme = getMuiTheme(lightBaseTheme);
+    const wrapper = mount(<AppBar muiTheme={muiTheme} />, {
+      childContextTypes: {
+        muiTheme: object.isRequired
+      },
+      context: {
+        muiTheme,
+        store
+      }
+    });
+
+    wrapper.find('AppBar')
+      .props()
+      .onTitleTouchTap();
+
+    const [{payload}] = store.getActions();
+
+    expect(payload).toEqual({
+      method: 'push',
+      args: ['/home']
+    });
   });
 });
