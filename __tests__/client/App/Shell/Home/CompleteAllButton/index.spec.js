e09455695ec1197272d38a3a16e0e029534fe2a6
Noam Okman <noamokman@gmail.com>
2017-08-23 11:03:43 +0300

    test(client): CompleteAllButton container

--- __tests__/client/App/Shell/Home/CompleteAllButton/index.spec.js
+++ __tests__/client/App/Shell/Home/CompleteAllButton/index.spec.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import CompleteAllButton from '../../../../../../client/App/Shell/Home/CompleteAllButton';
 import configureMockStore from 'redux-mock-store';
+import {object} from 'prop-types';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 
 describe('CompleteAllButton container', () => {
   it('renders without crashing', () => {
@@ -9,5 +12,25 @@ describe('CompleteAllButton container', () => {
     const store = mockStore({home: {todos: []}});
 
     shallow(<CompleteAllButton store={store} />);
+  });
+
+  it('should handle click', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore({home: {todos: [{_id: '1', completed: false}]}});
+    const muiTheme = getMuiTheme(lightBaseTheme);
+    const wrapper = mount(<CompleteAllButton muiTheme={muiTheme} store={store} />, {
+      childContextTypes: {
+        muiTheme: object.isRequired
+      },
+      context: {muiTheme}
+    });
+
+    wrapper.find('IconButton')
+      .props()
+      .onClick();
+
+    const [{payload: {request: {data}}}] = store.getActions();
+
+    expect(data).toHaveProperty('completed', true);
   });
 });
