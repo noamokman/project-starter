b8cb8cffeabf13d6cef925ae1542cfe40d8c1d59
Sharon Grossman <sharong1337@gmail.com>
2017-08-24 09:57:26 +0300

    test(login): added tests for the login container

--- __tests__/client/App/Exterior/Login/index.spec.js
+++ __tests__/client/App/Exterior/Login/index.spec.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Login from '../../../../../client/App/Exterior/Login';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import configureMockStore from 'redux-mock-store';
+import {multiClientMiddleware} from 'redux-axios-middleware';
+import {object} from 'prop-types';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import promiseMiddleware from 'redux-simple-promise';
+import {createAxiosConfig} from '../../../../../client/create-store';
 
 describe('Login container', () => {
   it('renders without crashing', () => {
@@ -9,5 +15,44 @@ describe('Login container', () => {
     const store = mockStore({auth: {}, login: {}});
 
     shallow(<Login store={store} />);
+  });
+
+  it('login function dispatches correctly', () => {
+    const mockStore = configureMockStore([
+      promiseMiddleware(),
+      multiClientMiddleware(createAxiosConfig())
+    ]);
+    const store = mockStore({auth: {}, login: {}});
+    const muiTheme = getMuiTheme(lightBaseTheme);
+    const wrapper = mount(<Login muiTheme={muiTheme} store={store} />, {
+      childContextTypes: {
+        muiTheme: object.isRequired
+      },
+      context: {muiTheme, store}
+    });
+
+    wrapper.find('form')
+      .props()
+      .onSubmit();
+  });
+
+  it('load user occurs if there is a token', () => {
+    const mockStore = configureMockStore([
+      promiseMiddleware(),
+      multiClientMiddleware(createAxiosConfig())
+    ]);
+    const store = mockStore({auth: {token: 'token'}, login: {}});
+    const muiTheme = getMuiTheme(lightBaseTheme);
+
+    mount(<Login muiTheme={muiTheme} store={store} />, {
+      childContextTypes: {
+        muiTheme: object.isRequired
+      },
+      context: {muiTheme, store}
+    });
+
+    const action = store.getActions().filter(({type}) => type === 'LOAD_USER');
+
+    expect(action).toBeTruthy();
   });
 });
