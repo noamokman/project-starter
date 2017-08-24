31b287e099573414c0793b41a5549a1f309d8743
Noam Okman <noamokman@gmail.com>
2017-08-24 17:05:46 +0300

    test(client): fix test errors

--- __tests__/client/components/DevTools/index.spec.js
+++ __tests__/client/components/DevTools/index.spec.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import DevTools from '../../../../client/components/DevTools';
 import {shallow} from 'enzyme';
+import {createStore, compose} from 'redux';
 
 describe('DevTools component', () => {
   it('renders without crashing', () => {
-    shallow(<DevTools />);
+    const store = createStore((state = {}) => state, {}, compose(DevTools.instrument()));
+
+    shallow(<DevTools store={store} />);
   });
 });
--- __tests__/client/components/Root/index.spec.js
+++ __tests__/client/components/Root/index.spec.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Root from '../../../../client/components/Root';
 import {shallow} from 'enzyme';
+import configureMockStore from 'redux-mock-store';
 
 describe('Root component', () => {
   it('renders without crashing', () => {
-    shallow(<Root />);
+    const mockStore = configureMockStore();
+    const store = mockStore({});
+
+    shallow(<Root store={store} />);
   });
 });
