c954c879c673ef652cbe41a37d51b5f3cd96ad05
Noam Okman <noamokman@gmail.com>
2017-08-25 12:43:59 +0300

    chore(plop): added socket tests for api plop

--- plop/api/index.js
+++ plop/api/index.js
@@ -25,61 +25,75 @@ export default plop => {
         message: 'Do you want to add sockets?'
       }
     ],
-    actions: [
-      {
-        type: 'add',
-        path: 'server/api/{{name}}/index.js',
-        templateFile: 'plop/api/index.hbs'
-      },
-      {
-        type: 'add',
-        path: 'server/api/{{name}}/{{name}}.controller.js',
-        templateFile: 'plop/api/controller.hbs'
-      },
-      {
-        type: 'add',
-        path: 'server/api/{{name}}/{{name}}.model.js',
-        templateFile: 'plop/api/model.hbs'
-      },
-      {
-        type: 'add',
-        path: '__tests__/server/api/{{name}}.spec.js',
-        templateFile: 'plop/api/spec.hbs'
-      },
-      {
-        type: 'add',
-        path: 'server/api/{{name}}/{{name}}.seed.js',
-        templateFile: 'plop/api/seed.hbs'
-      },
-      {
-        type: 'add',
-        path: 'server/api/{{name}}/{{name}}.socket.js',
-        templateFile: 'plop/api/socket.hbs'
-      },
-      {
-        type: 'modify',
-        path: 'server/config/express/routes.js',
-        pattern: /(\/\/ inject:route-imports)/gi,
-        template: '$1\r\nimport {{name}}Route from \'../../api/{{name}}\';'
-      },
-      {
-        type: 'modify',
-        path: 'server/config/socket.js',
-        pattern: /(\/\/ inject:socket-imports)/gi,
-        template: '$1\r\nimport {{name}}Socket from \'../api/{{name}}/{{name}}.socket\';'
-      },
-      {
-        type: 'modify',
-        path: 'server/config/express/routes.js',
-        pattern: /(\/\/ inject:route-usage)/gi,
-        template: '$1\r\n  app.use(\'/api/{{plur name}}\', {{name}}Route);'
-      },
-      {
-        type: 'modify',
-        path: 'server/config/socket.js',
-        pattern: /(\/\/ inject:socket-usage)/gi,
-        template: '$1\r\n  {{name}}Socket(getSockets);'
+    actions: ({socket}) => {
+      const actions = [
+        {
+          type: 'add',
+          path: 'server/api/{{name}}/index.js',
+          templateFile: 'plop/api/index.hbs'
+        },
+        {
+          type: 'add',
+          path: 'server/api/{{name}}/{{name}}.controller.js',
+          templateFile: 'plop/api/controller.hbs'
+        },
+        {
+          type: 'add',
+          path: 'server/api/{{name}}/{{name}}.model.js',
+          templateFile: 'plop/api/model.hbs'
+        },
+        {
+          type: 'add',
+          path: '__tests__/server/api/{{name}}.spec.js',
+          templateFile: 'plop/api/spec.hbs'
+        },
+        {
+          type: 'add',
+          path: 'server/api/{{name}}/{{name}}.seed.js',
+          templateFile: 'plop/api/seed.hbs'
+        },
+        {
+          type: 'modify',
+          path: 'server/config/express/routes.js',
+          pattern: /(\/\/ inject:route-imports)/gi,
+          template: '$1\r\nimport {{name}}Route from \'../../api/{{name}}\';'
+        },
+        {
+          type: 'modify',
+          path: 'server/config/express/routes.js',
+          pattern: /(\/\/ inject:route-usage)/gi,
+          template: '$1\r\n  app.use(\'/api/{{plur name}}\', {{name}}Route);'
+        }
+      ];
+
+      if (socket) {
+        actions.push(
+          {
+            type: 'modify',
+            path: 'server/config/socket.js',
+            pattern: /(\/\/ inject:socket-usage)/gi,
+            template: '$1\r\n  {{name}}Socket(getSockets);'
+          },
+          {
+            type: 'modify',
+            path: 'server/config/socket.js',
+            pattern: /(\/\/ inject:socket-imports)/gi,
+            template: '$1\r\nimport {{name}}Socket from \'../api/{{name}}/{{name}}.socket\';'
+          },
+          {
+            type: 'add',
+            path: 'server/api/{{name}}/{{name}}.socket.js',
+            templateFile: 'plop/api/socket.hbs'
+          },
+          {
+            type: 'add',
+            path: '__tests__/server/api/{{name}}.socket.spec.js',
+            templateFile: 'plop/api/socket.spec.hbs'
+          }
+        );
       }
-    ]
+
+      return actions;
+    }
   });
 };
